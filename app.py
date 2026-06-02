671d4aea68e2f617dfb9313d94da22e3526fe155
raze-me <ac.otherside@gmail.com>
2026-06-03 00:17:00 +0530

    creating login page with google OAuth via firebase

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('imdex.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
